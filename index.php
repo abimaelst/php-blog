7ea6140be8d57e3b0a43ca70c73dc3b14f4bb99c
abimael.tavares <abimael.tavares@devsquad.com>
2023-06-01 23:58:43 -0300

    adding a route

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 
-$heading = 'Home';
+require "functions.php";
 
-function dd($value) {
-    echo '<pre>';
-    var_dump($value);
-    echo '</pre>';
+$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    '/' => 'controllers/index.php',
+    '/about' => 'controllers/about.php',
+    '/contact' => 'controllers/contact.php',
+];
+
+if(array_key_exists($uri, $routes)){
+    require $routes[$uri];
+} else {
+    http_response_code('404');
+    require "views/404.php";
     die();
 }
 
-require "views/index.view.php";
