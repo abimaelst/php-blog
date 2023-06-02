dc4b78341b04eab0742965aad65bb50114c294ad
abimael.tavares <abimael.tavares@devsquad.com>
2023-06-01 22:48:29 -0300

    creating partials

--- views/index.view.php
+++ views/index.view.php
@@ -1,26 +1,9 @@
-<!doctype html>
-<html lang="en" class="h-full bg-gray-100">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdn.tailwindcss.com"></script>
-    <title>Demos</title>
-</head>
-<body class="h-full">
-    <div class="min-h-full">
-        <?php require('partials/nav.php') ?>
-        <header class="bg-white shadow">
-            <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                <h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
-            </div>
-        </header>
-        <main>
-            <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-                <!-- Your content -->
-            </div>
-        </main>
-    </div>
-</body>
-</html>
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+<?php require('partials/banner.php') ?>
+    <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+            <!-- Your content -->
+        </div>
+    </main>
+<?php require('partials/footer.php') ?>
