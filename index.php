7b824877a7d1e489fabdd01d196546874b4e9329
abimael.tavares <abimael.tavares@devsquad.com>
2023-06-02 00:09:01 -0300

    creating a new file for route

--- index.php
+++ index.php
@@ -2,19 +2,5 @@
 
 require "functions.php";
 
-$uri = parse_url($_SERVER['REQUEST_URI'])['path'];
-
-$routes = [
-    '/' => 'controllers/index.php',
-    '/about' => 'controllers/about.php',
-    '/contact' => 'controllers/contact.php',
-];
-
-if(array_key_exists($uri, $routes)){
-    require $routes[$uri];
-} else {
-    http_response_code('404');
-    require "views/404.php";
-    die();
-}
+require "route.php";
 
